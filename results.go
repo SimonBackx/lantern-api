5daafa187dd99c56f9ef49e0f93b2d833f337dff
Simon Backx <hi@simonbackx.com>
2017-05-06 16:21:51 +0200

    Aggragated results

--- results.go
+++ results.go
@@ -9,10 +9,17 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
+type AggregatedResult struct {
+	Id        string    `json:"_id" bson:"_id"`
+	LastFound time.Time `json:"lastFound" bson:"lastFound"`
+	Count     int       `json:"count" bson:"count"`
+}
+
 /**
- * /results/{queryId}
+ * /results/{queryId}[?host=...]
  */
 func resultsHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -24,10 +31,43 @@ func resultsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	queryIdBson := bson.ObjectIdHex(queryId)
-
 	c := mongo.DB("lantern").C("results")
+
+	queryValues := r.URL.Query()
+	hostArr, found := queryValues["host"]
+	if !found || len(hostArr) == 0 {
+
+		// Accumuleren
+		pipe := c.Pipe(
+			[]bson.M{
+				{"$match": bson.M{"queryId": queryIdBson}},
+				{"$group": bson.M{"_id": "$host", "count": bson.M{"$sum": 1}, "lastFound": bson.M{"$max": "$lastFound"}}},
+				{"$sort": bson.M{"lastFound": -1}},
+			})
+		iter := pipe.Iter()
+
+		var result []AggregatedResult
+		err := iter.All(&result)
+		if err != nil {
+			internalErrorHandler(w, r, err)
+			return
+		}
+
+		jsonValue, err := json.Marshal(result)
+		if err != nil {
+			internalErrorHandler(w, r, err)
+			return
+		}
+
+		fmt.Fprintf(w, "%s", jsonValue)
+		return
+	}
+
+	host := hostArr[0]
+
+	// Specifieke host
 	var result []queries.Result
-	err := c.Find(bson.M{"queryId": queryIdBson}).Select(bson.M{"_id": 1, "queryId": 1, "lastFound": 1, "createdOn": 1, "occurrences": 1, "url": 1, "snippet": 1, "title": 1, "host": 1}).Sort("-lastFound").Limit(500).All(&result)
+	err := c.Find(bson.M{"queryId": queryIdBson, "host": host}).Select(bson.M{"_id": 1, "queryId": 1, "lastFound": 1, "createdOn": 1, "occurrences": 1, "url": 1, "snippet": 1, "title": 1, "host": 1}).Sort("-lastFound").Limit(500).All(&result)
 	if err != nil {
 		internalErrorHandler(w, r, err)
 		return
@@ -84,6 +124,11 @@ func resultHandler(w http.ResponseWriter, r *http.Request) {
  */
 func newResultHandler(w http.ResponseWriter, r *http.Request) {
 	str, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		internalErrorHandler(w, r, err)
+		return
+	}
 
 	var result queries.Result
 	err = json.Unmarshal(str, &result)
